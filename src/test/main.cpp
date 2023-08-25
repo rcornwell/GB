849bd08c28c4753f9021f2447accd6b3d8fb1011
Richard Cornwell <rich@sky-visions.com>
2023-08-25 15:03:38 -0400

    Added acid display tests to test suite.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -29,6 +29,7 @@
 #include <chrono>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -44,7 +45,8 @@ using namespace std;
 bool        trace_flag = false;
 Joypad      *joy;
 
-uint8_t    lcd_panel[160][144];
+uint8_t    lcd_panel[144][160];
+int        vblank;
 
 void init_window()
 {
@@ -52,10 +54,12 @@ void init_window()
 
 void init_screen()
 {
+    memset(lcd_panel, 0xff, sizeof(lcd_panel));
 }
 
 void draw_screen()
 {
+    vblank++;
 }
 
 void set_joypad(Joypad *_joy)
@@ -91,10 +95,98 @@ extern uint8_t  instr_timing[];
 extern size_t   instr_timing_sz;
 extern uint8_t  mem_timing[];
 extern size_t   mem_timing_sz;
+extern uint8_t  dmg_acid_screen[144][160];
+extern uint8_t  cgb_acid_screen[144][160];
 
 TEST_GROUP(CPU)
 {
 };
+
+TEST(CPU, cgb_acid)
+{
+    /* Open up and read in ROM file */
+     std::ifstream inputFile("cgb-acid2.gbc", std::ios::binary | std::ios::in);
+
+     if (!inputFile) {
+        std::cerr << "Failed to open file: cgb-acid2.gbc" << std::endl;
+        return;
+     }
+
+     inputFile.seekg(0, inputFile.end);
+     size_t rom_size = inputFile.tellg();
+     inputFile.seekg(0, inputFile.beg);
+
+     uint8_t *rom = new uint8_t[rom_size];
+
+     inputFile.read((char *)rom, rom_size);
+     inputFile.close();
+
+     std::cerr << "Read " << (int)rom_size << " bytes from cgb-acid2.gbc"
+                << std::endl;
+
+
+     Cartridge     cart(rom, rom_size, false);
+
+     Cpu          *cpu = new Cpu(&cart, true);
+
+     cpu->run();
+     vblank = 0;
+
+     while (vblank <= 340) {
+          cpu->step();
+     }
+
+     for (int i = 0; i < 144; i++) {
+         for (int j = 0; j < 160; j++) {
+             CHECK_EQUAL (lcd_panel[i][j], cgb_acid_screen[i][j]);
+         }
+     }
+     delete cpu;
+     delete[] rom;
+}
+
+TEST(CPU, dmg_acid)
+{
+    /* Open up and read in ROM file */
+     std::ifstream inputFile("dmg-acid2.gb", std::ios::binary | std::ios::in);
+
+     if (!inputFile) {
+        std::cerr << "Failed to open file: dmg-acid2.gb" << std::endl;
+        return;
+     }
+
+     inputFile.seekg(0, inputFile.end);
+     size_t rom_size = inputFile.tellg();
+     inputFile.seekg(0, inputFile.beg);
+
+     uint8_t *rom = new uint8_t[rom_size];
+
+     inputFile.read((char *)rom, rom_size);
+     inputFile.close();
+
+     std::cerr << "Read " << (int)rom_size << " bytes from dmg-acid2.gb"
+                << std::endl;
+
+
+     Cartridge     cart(rom, rom_size, false);
+
+     Cpu          *cpu = new Cpu(&cart, false);
+
+     cpu->run();
+     vblank = 0;
+
+     while (vblank <= 340) {
+          cpu->step();
+     }
+
+     for (int i = 0; i < 144; i++) {
+         for (int j = 0; j < 160; j++) {
+             CHECK_EQUAL (lcd_panel[i][j] , dmg_acid_screen[i][j]);
+         }
+     }
+     delete cpu;
+     delete[] rom;
+}
 
 TEST(CPU, memtiming)
 {
@@ -126,7 +218,7 @@ TEST(CPU, timing)
 
 TEST(CPU, instr)
 {
-    uint64_t    tim = 0;
+    uint64_t    n_cycles = 0;
     uint64_t    n_inst = 0;
 
     Cartridge     cart(&cpu_inst[0], cpu_inst_sz, false);
@@ -137,16 +229,16 @@ TEST(CPU, instr)
          cpu->step();
          n_inst++;
     }
-    tim = cpu->get_cycles();
+    n_cycles = cpu->get_cycles();
     cout << endl;
     auto end = chrono::high_resolution_clock::now();
-    cout << "Simulated time: " << dec << tim << endl;
+    cout << "Simulated time: " << dec << n_cycles << endl;
     cout << "Excuted: " << n_inst << endl;
     auto s = chrono::duration_cast<chrono::seconds>(end - start);
     cout << "Run time: " << s.count() << " seconds" << endl;
     auto ctim = chrono::duration_cast<chrono::nanoseconds>(end - start);
     cout << "Time: " << ctim.count() << " ns" << endl;
-    int cy  = ((ctim.count()) / (tim));
+    int cy  = ((ctim.count()) / (n_cycles));
     cout << "Cycle time: " << (cy / 10) << "." << (cy % 10) << " ns" << endl;
     cout << "Instruct time: " << (ctim.count() / n_inst) << " ns" << endl;
 
